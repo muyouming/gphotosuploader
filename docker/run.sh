92522050f42de5f17463c791891b08df39d32a78
garfieldkwong <garfieldkwong@gmail.com>
2020-05-15 16:40:46 +0800

    Remove cow

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,6 @@
 #! /bin/bash -eu
 
 echo fs.inotify.max_user_watches=1048576 | tee -a /etc/sysctl.conf && sysctl -p
-echo "listen = http://127.0.0.1:7777" > ~/.cow/rc
-echo "proxy = socks5://${SOCKS5_PROXY}" >> ~/.cow/rc
 echo "${COOKIES}" >> /go/src/app/auth.json
 ignore_patterns=(${IGNORE_PATTERN})
 ignore_arg=''
@@ -11,5 +9,4 @@ do
     ignore_arg="$ignore_arg --ignore $val"
 done
 echo "Run gphotosUploader $ignore_arg"
-/bin/bash -c "nohup ./cow & "
-/bin/bash -c "sleep 4; /go/bin/gphotosuploader --watch /photo --maxConcurrent 4 $ignore_arg" 
+/bin/bash -c "/go/bin/gphotosuploader --watch /photo --maxConcurrent 4 $ignore_arg" 
