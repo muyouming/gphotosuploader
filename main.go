5975771eeea50dc2ec902178b40b32e26b6cf7fe
garfieldkwong <garfieldkwong@gmail.com>
2020-05-25 01:00:18 +0800

    Delete file record when received delete or rename notification

--- main.go
+++ main.go
@@ -284,6 +284,8 @@ func handleFileChange(event fsnotify.Event, fsWatcher *fsnotify.Watcher) {
 			}
 		})
 		timers[event.Name] = timer
+	} else if event.Op == fsnotify.Rename || event.Op == fsnotify.Remove {
+		orm.GetInstance().Connection.Where("path = ?", event.Name).Delete(&models.File{})
 	}
 }
 
